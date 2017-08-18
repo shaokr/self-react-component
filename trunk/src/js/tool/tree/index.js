3a9adfc940d4161e9d021c77fb9a187ab022c530
shaokr <shaokangrun@91kook.com>
2017-08-18 10:30:01 +0800

    工具修正

--- trunk/src/js/tool/tree/index.js
+++ trunk/src/js/tool/tree/index.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import _ from 'lodash';
 import Tree from 'zy-tree';
 import Apiutil from 'Apiutil';
+import param from 'util/widget/param';
 
 // 'Apiutil': 'host:js/Apiutil-util/1.0.0/util.min.js',
 // const cdnHost = '//192.168.1.251/fed/web-cdn';
@@ -25,169 +26,249 @@ import Apiutil from 'Apiutil';
  * 获取部门信息
  * @param {*} param0
  */
-export const getDept = async ({ did }, ck) => {
+export const getDept = async ({ key }, ck) => {
     const GsInfo = await Apiutil.fetch('manage.dept.get', {
-        did
+        did: key
     }).then((res) => {
         const [data] = res.datas;
         return data;
     });
+    if (typeof ck === 'function') {
+        ck(GsInfo);
+    }
     return GsInfo;
 };
+
 /**
  * 获取用户列表
  * @param {*} data
  */
-export const getUserList = async (data, ck) => {
-    if (!data.children) {
-        const did = data.key;
-        const userList = await Apiutil.fetch('manage.user.list', { did }).then(res =>
-            _.map(res.datas, item => (
-                {
-                    key: item.uid,
-                    name: item.name
-                }
-            ))
-        );
+export const getUserList = async ({ key, type = 'user' }, ck) => {
+    const did = key;
+    const userList = await Apiutil.fetch('manage.user.list', { did }).then(res =>
+        _.map(res.datas, item => (
+            {
+                key: item.uid,
+                name: item.name,
+                type
+            }
+        ))
+    );
 
-        const RData = userList;
-        return RData;
+    const RData = userList;
+    if (typeof ck === 'function') {
+        ck(RData);
     }
-    return false;
+    return RData;
 };
 /**
  * 获取部门列表
  * @param {*} data
  */
-export const getDeptList = async (data, ck) => {
-    if (!data.children) {
-        const did = data.key;
-        const deptList = await Apiutil.fetch('manage.dept.list', { did }).then(res =>
-            _.map(res.datas, item => (
-                {
-                    key: item.did,
-                    name: item.name,
-                    isChildren: true,
-                        // has_child,
-                    icon: 'folder'
-                }
-            ))
-        );
+export const getDeptList = async ({ key, type = 'dept', children }, ck) => {
+    const did = key;
+    const deptList = await Apiutil.fetch('manage.dept.list', { did }).then(res =>
+        _.map(res.datas, (item) => {
+            const _item = _.find(children, ['key', item.did]) || {};
+            return {
+                key: item.did,
+                name: item.name,
+                isChildren: true,
+                    // has_child,
+                icon: 'folder',
+                type,
+                children: _item.children
+            };
+        })
+    );
 
-        const RData = deptList;
-        return RData;
+    const RData = deptList;
+    if (typeof ck === 'function') {
+        ck(RData);
     }
-    return false;
+    return RData;
 };
 /**
  * 获取部门和用户
  * @param {*} data
  */
 export const getDeptAndUserList = async (data, ck) => {
+    const _data = _.assign({}, data, { type: 'dept-user' });
     const [userList, deptList] = await Promise.all([
-        getUserList(data),
-        getDeptList(data)
+        getUserList(_data),
+        getDeptList(_data)
     ]);
 
     const RData = [
         ...deptList,
         ...userList
     ];
+
+    if (typeof ck === 'function') {
+        ck(RData);
+    }
     return RData;
 };
 
 /**
  * 获取搜索数据
  */
-export const getSearch = async (val, ck) => {
-    const res = await Apiutil.fetch('search.user.get', {
-        keyword: val
-    });
-    if (res.err_code === '0') {
-        let list = [];
-        _.forEach(res.hits, (item) => {
-            const _item = {
-                name: item.name,
-                key: item.uid
-            };
-            list.push(
-                ..._.map(item.depts, dept => ({
-                    ..._item,
-                    title: dept.name.split('-')[0]
+export const getSearch = async (data, ck) => {
+    let _data = {};
+    if (typeof data === 'string') {
+        _data.keyword = data;
+    }
+    if (typeof data === 'object') {
+        _data = {
+            ...data,
+            keyword: data.key
+        };
+    }
+    if (_data.keyword) {
+        const res = await Apiutil.fetch('search.user.get', _data);
+        if (res.err_code === '0') {
+            let list = [];
+            _.forEach(res.hits, (item) => {
+                const _item = {
+                    name: item.name,
+                    key: item.uid
+                };
+                list.push(
+                    ..._.map(item.depts, dept => ({
+                        ..._item,
+                        title: dept.name.split('-')[0]
 
-                }))
-            );
-        });
-        list = _.groupBy(list, 'title');
-
-        if (ck) {
-            
+                    }))
+                );
+            });
+            list = _.groupBy(list, 'title');
+            const RData = _.map(list, (item, key) => ({
+                title: key,
+                children: item
+            }));
+            if (typeof ck === 'function') {
+                ck(RData);
+            }
+            return RData;
         }
-        return _.map(list, (item, key) => ({
-            title: key,
-            children: item
-        }));
+    }
+};
+/**
+ * 获取群用户系列
+ */
+export const getGroup = async ({ key, type = 'group' }, ck) => {
+    const res = await Apiutil.fetch('manage.group.getUserlist', {
+        group_id: key
+    });
+
+    const Rdata = _.map(res.datas, item => ({
+        key: item.uid,
+        name: item.name,
+        type
+    }));
+    if (ck) {
+        ck(Rdata);
+    }
+    return Rdata;
+};
+
+export const onExpand = async (data, ck) => {
+    if (data.type === 'group') {
+        return getGroup(data, ck);
+    }
+    if (data.type === 'dept-user') {
+        return getDeptAndUserList(data, ck);
+    }
+    if (data.type === 'dept') {
+        return getDeptList(data, ck);
+    }
+    if (data.type === 'user') {
+        return getUserList(data, ck);
     }
 };
 
-(async () => {
-    // await Systemjs.import(`${cdnHost}/config/1.0.6/config.js`);
-    Apiutil.setUrl('192.168.1.239:82');
-    const loginRes = await Apiutil.fetch('im.login.signin', {
-        login_type: 1,
-        account: 'guanliyuan',
-        password: '111111',
-        verify_code: '1.0',
-        client_ver: '0.1.0',
-        dev_type: 5
+/**
+ * 获取初始的根部门和用户信息
+ */
+export const initData = async (data = {}) => {
+    const { key = '0', type = 'dept-user' } = data;
+    const GsInfo = await getDept({ key });
+    const list = await onExpand({
+        key: GsInfo.did,
+        type
     });
-
-    Apiutil.setData({
-        access_token: loginRes.access_token,
-        cid: loginRes.cids[0],
-        client_ver: '1.0.0',
-        dev_type: '5',
-        operator_uid: loginRes.uid,
-        uid: loginRes.uid
-    });
-    const GsInfo = await getDept({ did: 0 });
-    const list = await getDeptAndUserList({
-        key: GsInfo.did
-    });
-
-    const tree = [
-        {
-            key: GsInfo.did,
-            name: GsInfo.name,
-            isChildren: true,
-            icon: 'company',
-            expand: true,
-            children: list
-        }
-    ];
-    const _getSearch = async (val, ck) => {
-        const res = await getSearch(val);
-        ck(res);
+    return {
+        key: GsInfo.did,
+        name: GsInfo.name,
+        isChildren: true,
+        type,
+        icon: GsInfo.did === '0' ? 'company' : 'folder',
+        expand: true,
+        children: list
     };
-    const _getDeptAndUserList = async (val, ck) => {
-        const res = await getDeptAndUserList(val);
-        ck(res);
-    };
-    // return Apiutil;
-    render(<Tree
-        isIntegration
-        onClickBtn={(...res) => {
-            console.log(res);
-        }}
-        expandType="1"
-        onSearchChange={_getSearch}
-                // consfig
-                // type="radio"
-        tree={tree}
-        onExpand={_getDeptAndUserList}
-    />, document.getElementById('app-main'));
-})();
+};
 
+if (__DEV__) {
+    if (param.debug == 1) {
+        (async () => {
+            // await Systemjs.import(`${cdnHost}/config/1.0.6/config.js`);
+            Apiutil.setUrl('192.168.1.251:82');
+            const loginRes = await Apiutil.fetch('im.login.signin', {
+                login_type: 1,
+                account: 'guanliyuan',
+                password: '111111',
+                verify_code: '1.0',
+                client_ver: '0.1.0',
+                dev_type: 5
+            });
 
-// export default {
-// };
+            Apiutil.setData({
+                access_token: loginRes.access_token,
+                cid: loginRes.cids[0],
+                client_ver: '1.0.0',
+                dev_type: '5',
+                operator_uid: loginRes.uid,
+                uid: loginRes.uid
+            });
+
+            const companyData = await initData({ type: 'user' });
+            const groupIdData = await getGroup({ key: '115' });
+            const tree = [
+                companyData,
+                {
+                    key: '115',
+                    name: '群12312312qw',
+                    type: 'group',
+                    isChildren: true,
+                    children: groupIdData,
+                    childrenNumber: 111
+                }
+            ];
+            // return Apiutil;
+            render(<Tree
+                isIntegration
+                onClickBtn={(...res) => {
+                    console.log(res);
+                }}
+                expandType="1"
+                onSearchChange={getSearch}
+                        // consfig
+                        // type="radio"
+                tree={tree}
+                onExpand={onExpand}
+                max="7"
+            />, document.getElementById('app-main'));
+        })();
+    }
+}
+
+export default {
+    getDept,
+    getUserList,
+    getDeptList,
+    getDeptAndUserList,
+    getSearch,
+    getGroup,
+    onExpand,
+    initData
+};
