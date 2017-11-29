93b0d9408a8570751aec01bc73826ee571d0235f
shaokr <shaokangrun@91kook.com>
2017-11-29 14:01:47 +0800

    配置

--- src/js/config/const.js
+++ src/js/config/const.js
@@ -1,6 +1,31 @@
 /**
  * 常量配置
  */
-module.exports = {
-    prefix: 'zy' // 前缀
+export const prefix = 'zy';
+export const prefixModal = `${prefix}-modal`;
+export const prefixButton = `${prefix}-button`;
+export const prefixIcon = `${prefix}-icon`;
+export const prefixMessage = `${prefix}-message`;
+export const prefixTree = `${prefix}-tree`;
+export const prefixLoading = `${prefix}-loading`;
+export const prefixCheckbox = `${prefix}-checkbox`;
+export const prefixScrollbar = `${prefix}-scrollbar`;
+export const prefixWatermark = `${prefix}-watermark`;
+export const prefixDatePicker = `${prefix}-datePicker`;
+export const prefixCalendar = `${prefix}-calendar`;
+
+
+export default {
+    prefix,
+    prefixModal,
+    prefixButton,
+    prefixIcon,
+    prefixMessage,
+    prefixTree,
+    prefixLoading,
+    prefixCheckbox,
+    prefixScrollbar,
+    prefixWatermark,
+    prefixDatePicker,
+    prefixCalendar
 };
