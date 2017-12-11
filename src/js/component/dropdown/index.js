027685d21cfb3cae91c153f2d523e562dcd0dd35
shaokr <shaokangrun@91kook.com>
2017-12-11 16:20:36 +0800

    一些修改

--- src/js/component/dropdown/index.js
+++ src/js/component/dropdown/index.js
@@ -7,14 +7,15 @@ import fp from 'lodash/fp';
 
 import classnames from 'classnames';
 import PromiseClass from 'util/promise-class';
-
 import { prefixDropdown } from 'config/const';
+
+import documentOn from 'helpers/document-on';
+
+
 import superDom from '../super-dom';
-import lessConst from 'less/global/var.less';
 
 import './index.less';
 
-console.log(lessConst);
 const placementConfig = [
     'bottomLeft',
     'bottomCenter',
@@ -95,9 +96,11 @@ class OverlayMain extends Component {
         );
     }
 }
+
 /**
  * 下拉主体
  */
+@documentOn(['click'])
 export default class Dropdown extends Component {
     constructor(props) {
         super(props);
@@ -106,12 +109,62 @@ export default class Dropdown extends Component {
             overlayShow: false
         };
 
+        this.invokeProps = this.invokeProps.bind(this);
+        this.onClick = this.onClick.bind(this);
         this.onSetOverlay = this.onSetOverlay.bind(this);
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave = this.onMouseLeave.bind(this);
+        this.documentClick = this.documentClick.bind(this);
     }
-    onSetOverlay(state = true) {
+    /**
+     * 统一修改显示入口
+     * @param {*} overlayShow 设置overlayShow的值
+     */
+    onSetOverlay(overlayShow = !this.state.overlayShow) {
+        this.invokeProps('onVisibleChange', overlayShow);
         this.setState({
-            overlayShow: state
+            overlayShow
         });
+    }
+    // 鼠标移入事件
+    onMouseEnter(e) {
+        this.invokeProps('onMouseEnter', e);
+        if (this.props.trigger === 'hover') {
+            this.onSetOverlay(true);
+        }
+    }
+    // 鼠标移出事件
+    onMouseLeave(e) {
+        if (this.props.trigger === 'hover') {
+            this.onSetOverlay(false);
+        }
+        this.invokeProps('onMouseLeave', e);
+    }
+    // 点击事件
+    onClick(e) {
+        if (this.props.trigger === 'click') {
+            this.onSetOverlay();
+        }
+        this.invokeProps('onClick', e);
+    }
+    // document中点击事件
+    /**
+     * @param {*} e
+     * @param {Boolean} contains 是否是自己的子元素
+     */
+    documentClick(e, contains) {
+        if (this.props.trigger === 'click' && !contains) {
+            this.onSetOverlay(false);
+        }
+    }
+    /**
+     * 执行props上的方法
+     * @param {*} key props中的key
+     * @param {*} req 参数
+     */
+    invokeProps(key, ...args) {
+        const _fun = _.get(this, ['props', key]);
+        if (_.isFunction(_fun)) _fun(...args);
     }
     mainDom = new PromiseClass()
     get className() {
@@ -121,17 +174,25 @@ export default class Dropdown extends Component {
             props.className
         ]);
     }
+    get overlayShow() {
+        const { visible } = this.props;
+        if (_.isUndefined(visible)) {
+            return this.state.overlayShow;
+        }
+        return !!visible;
+    }
     render() {
-        const { props, mainDom, state } = this;
+        const { props, mainDom } = this;
         return (
             <div
-                ref={mainDom.resolve}
+                ref={(d) => { mainDom.resolve(d); }}
                 className={this.className}
-                onMouseEnter={() => this.onSetOverlay(true)}
-                onMouseLeave={() => this.onSetOverlay(false)}
+                onClick={this.onClick}
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
             >
                 {props.children}
-                <OverlayMain visible={state.overlayShow} getContainer={props.getContainer} placement={props.placement} parentDom={mainDom.promise} >
+                <OverlayMain visible={this.overlayShow} getContainer={props.getContainer} placement={props.placement} parentDom={mainDom.promise} >
                     {props.overlay}
                 </OverlayMain>
             </div>
@@ -141,10 +202,10 @@ export default class Dropdown extends Component {
 
 Dropdown.defaultProps = {
     disabled: '', //	菜单是否禁用	boolean	-
-    // getContainer: '', //	菜单渲染父节点。默认渲染到 body 上，如果你遇到菜单滚动定位问题，试试修改为滚动的区域，并相对其定位。示例	Function(triggerNode)	() => document.body
+    getContainer: '', //	菜单渲染父节点。默认渲染到 body 上，如果你遇到菜单滚动定位问题，试试修改为滚动的区域，并相对其定位。示例	Function(triggerNode)	() => document.body
     overlay: '', //	菜单	Menu	-
     placement: defaultPlacementConfig, //	菜单弹出位置：bottomLeft bottomCenter bottomRight topLeft topCenter topRight	String	bottomLeft
-    trigger: '', //	触发下拉的行为	Array<'click'|'hover'>	'hover'
-    visible: '', //	菜单是否显示	boolean	-
+    trigger: 'hover', //	触发下拉的行为	Array<'click'|'hover'>	'hover'
+    visible: undefined, //	菜单是否显示	boolean	-
     onVisibleChange: '' // 菜单显示状态改变时调用，参数为 visible	Function(visible)	-
 };
