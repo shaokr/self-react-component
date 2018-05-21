bc509f0495bfd44eeda14ea5af3eb6aa07172e29
shaokr <shaokangrun@91kook.com>
2018-05-21 15:07:02 +0800

    去掉无用代码

--- src/js/tool/tree/react-web-module.js
+++
@@ -1,7 +0,0 @@
-import Tree from './react-main';
-import TreeTool from './web-module';
-
-export default (props) => {
-    const { io } = props;
-    return <Tree {...props} api={new TreeTool({ io })} />;
-};
