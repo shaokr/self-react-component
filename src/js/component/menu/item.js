4b0aaf7532154f89318970990d7bdd5006e41f35
shaokr <shaokangrun@91kook.com>
2018-07-04 15:13:17 +0800

    修改默认值

--- src/js/component/menu/item.js
+++ src/js/component/menu/item.js
@@ -10,30 +10,34 @@ import './item.less';
 import { prefixMenu } from 'config/const';
 
 export default class Item extends Component {
-    constructor(props) {
-        super(props);
-    }
-    get className() {
-        const { props } = this;
-        return classnames([
-            `${prefixMenu}--item`,
-            props.className,
-            {
-                [`${prefixMenu}--item__activ`]: props.activ
-            }
-        ]);
-    }
-    render() {
-        const { props } = this;
-        return (
-            <li onClick={props.onClick} className={this.className} onMouseOver={props.onMouseOver}>
-                {props.children}
-            </li>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  get className() {
+    const { props } = this;
+    return classnames([
+      `${prefixMenu}--item`,
+      props.className,
+      {
+        [`${prefixMenu}--item__activ`]: props.activ
+      }
+    ]);
+  }
+  render() {
+    const { props } = this;
+    return (
+      <li
+        onClick={props.onClick}
+        className={this.className}
+        onMouseOver={props.onMouseOver}
+      >
+        {props.children}
+      </li>
+    );
+  }
 }
 
 Item.defaultProps = {
-    activ: false,
-    onMouseOver: '' // 移动触发
+  activ: false,
+  onMouseOver: () => {} // 移动触发
 };
