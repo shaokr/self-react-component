64f610c952c716b50f427dd09ea84507cfba0f1b
shaokr <shaokangrun@91kook.com>
2018-07-04 15:12:18 +0800

    格式

--- src/js/component/dropdown/index.js
+++ src/js/component/dropdown/index.js
@@ -20,165 +20,162 @@ import './index.less';
  */
 @documentOn(['onClick', 'onContextMenu'])
 export default class Dropdown extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            overlayShow: props.defaultVisible, // false,
-            mouseType: {}
-        };
+    this.state = {
+      overlayShow: props.defaultVisible, // false,
+      mouseType: {}
+    };
 
-        this.invokeProps = this.invokeProps.bind(this);
-        this.onClick = this.onClick.bind(this);
-        this.onSetOverlay = this.onSetOverlay.bind(this);
-        this.onMouseEnter = this.onMouseEnter.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
-        this.documentOnClick = this.documentOnClick.bind(this);
-        this.onContextMenu = this.onContextMenu.bind(this);
-        this.documentOnContextMenu = this.documentOnContextMenu.bind(this);
+    this.invokeProps = this.invokeProps.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onSetOverlay = this.onSetOverlay.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.documentOnClick = this.documentOnClick.bind(this);
+    this.onContextMenu = this.onContextMenu.bind(this);
+    this.documentOnContextMenu = this.documentOnContextMenu.bind(this);
+  }
+  /**
+   * 统一修改显示入口
+   * @param {*} overlayShow 设置overlayShow的值
+   */
+  onSetOverlay(overlayShow = !this.state.overlayShow) {
+    if (overlayShow !== this.state.overlayShow) {
+      this.invokeProps('onVisibleChange', overlayShow);
+      this.setState({
+        overlayShow
+      });
     }
-    /**
-     * 统一修改显示入口
-     * @param {*} overlayShow 设置overlayShow的值
-     */
-    onSetOverlay(overlayShow = !this.state.overlayShow) {
-        if (overlayShow !== this.state.overlayShow) {
-            this.invokeProps('onVisibleChange', overlayShow);
-            this.setState({
-                overlayShow
-            });
-        }
+  }
+  // 鼠标移入事件
+  onMouseEnter(e) {
+    this.invokeProps('onMouseEnter', e);
+    if (this.props.trigger === 'hover') {
+      this.onSetOverlay(true);
     }
-    // 鼠标移入事件
-    onMouseEnter(e) {
-        this.invokeProps('onMouseEnter', e);
-        if (this.props.trigger === 'hover') {
-            this.onSetOverlay(true);
-        }
+  }
+  // 鼠标移出事件
+  onMouseLeave(e) {
+    if (this.props.trigger === 'hover') {
+      this.onSetOverlay(false);
     }
-    // 鼠标移出事件
-    onMouseLeave(e) {
-        if (this.props.trigger === 'hover') {
-            this.onSetOverlay(false);
-        }
-        this.invokeProps('onMouseLeave', e);
+    this.invokeProps('onMouseLeave', e);
+  }
+  // 点击事件
+  onClick(e) {
+    if (this.props.trigger === 'click') {
+      this.onSetOverlay();
+    } else {
+      this.onSetOverlay(false);
     }
-    // 点击事件
-    onClick(e) {
-        if (this.props.trigger === 'click') {
-            this.onSetOverlay();
-        } else {
-            this.onSetOverlay(false);
-        }
-        this.invokeProps('onClick', e);
+    this.invokeProps('onClick', e);
+  }
+  // 右键事件
+  onContextMenu(e) {
+    e.persist();
+    if (this.props.trigger === 'contextMenu') {
+      if (!this.state.overlayShow) {
+        this.onSetOverlay(true);
+      }
+      this.setMouseType(e);
     }
-    // 右键事件
-    onContextMenu(e) {
-        e.persist();
-        if (this.props.trigger === 'contextMenu') {
-            if (!this.state.overlayShow) {
-                this.onSetOverlay(true);
-            }
-            this.setMouseType(e);
-        }
-        this.invokeProps('onContextMenu', e);
-        return false;
-    }
-    setMouseType = (e) => {
-        this.mainDom.promise.then((dom) => {
-            if (dom.contains(e.target)) {
-                this.setState({
-                    mouseType: e
-                });
-            }
+    this.invokeProps('onContextMenu', e);
+    return false;
+  }
+  setMouseType = e => {
+    this.mainDom.promise.then(dom => {
+      if (dom.contains(e.target)) {
+        this.setState({
+          mouseType: e
         });
+      }
+    });
+  };
+  // document中点击事件
+  documentOnContextMenu(e, contains) {
+    if (!contains) {
+      this.onSetOverlay(false);
+    } else if (this.props.trigger === 'contextMenu') {
+      e.stopPropagation();
+      e.preventDefault();
+      return false;
     }
-    // document中点击事件
-    documentOnContextMenu(e, contains) {
-        if (!contains) {
-            this.onSetOverlay(false);
-        } else if (this.props.trigger === 'contextMenu') {
-            e.stopPropagation();
-            e.preventDefault();
-            return false;
-        }
+  }
+  /**
+   * @param {*} e
+   * @param {Boolean} contains 是否是自己的子元素
+   */
+  documentOnClick(e, contains) {
+    if (!contains) {
+      this.onSetOverlay(false);
     }
-    /**
-     * @param {*} e
-     * @param {Boolean} contains 是否是自己的子元素
-     */
-    documentOnClick(e, contains) {
-        if (!contains) {
-            this.onSetOverlay(false);
-        }
+  }
+  /**
+   * 执行props上的方法
+   * @param {*} key props中的key
+   * @param {*} req 参数
+   */
+  invokeProps(key, ...args) {
+    const _fun = _.get(this, ['props', key]);
+    if (_.isFunction(_fun)) _fun(...args);
+  }
+  mainDom = new PromiseClass();
+  get className() {
+    const { props } = this;
+    return classnames([prefixDropdown, props.className]);
+  }
+  get overlayShow() {
+    const { visible } = this.props;
+    if (_.isUndefined(visible)) {
+      return this.state.overlayShow;
     }
-    /**
-     * 执行props上的方法
-     * @param {*} key props中的key
-     * @param {*} req 参数
-     */
-    invokeProps(key, ...args) {
-        const _fun = _.get(this, ['props', key]);
-        if (_.isFunction(_fun)) _fun(...args);
+    return !!visible;
+  }
+  get placement() {
+    const { props } = this;
+    if (props.trigger === 'contextMenu') {
+      return 'mouse';
     }
-    mainDom = new PromiseClass()
-    get className() {
-        const { props } = this;
-        return classnames([
-            prefixDropdown,
-            props.className
-        ]);
-    }
-    get overlayShow() {
-        const { visible } = this.props;
-        if (_.isUndefined(visible)) {
-            return this.state.overlayShow;
-        }
-        return !!visible;
-    }
-    get placement() {
-        const { props } = this;
-        if (props.trigger === 'contextMenu') {
-            return 'mouse';
-        }
-        return props.placement;
-    }
-    render() {
-        const { state, props, mainDom } = this;
-        return (
-            <div
-                ref={d => (mainDom.resolve(d))}
-                className={this.className}
-                onClick={this.onClick}
-                onMouseEnter={this.onMouseEnter}
-                onMouseLeave={this.onMouseLeave}
-                onContextMenu={this.onContextMenu}
-                style={props.style}
-            >
-                {props.children}
-                <OverlayMain
-                    {...props.overlayProps}
-                    visible={this.overlayShow}
-                    getContainer={props.getContainer}
-                    placement={this.placement}
-                    parentDom={mainDom.promise}
-                    mouseType={state.mouseType}
-                >
-                    {props.overlay}
-                </OverlayMain>
-            </div>
-        );
-    }
+    return props.placement;
+  }
+  render() {
+    const { state, props, mainDom } = this;
+    return (
+      <div
+        ref={d => mainDom.resolve(d)}
+        className={this.className}
+        onClick={this.onClick}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        onContextMenu={this.onContextMenu}
+        style={props.style}
+      >
+        {props.children}
+        <OverlayMain
+          {...props.overlayProps}
+          visible={this.overlayShow}
+          getContainer={props.getContainer}
+          placement={this.placement}
+          parentDom={mainDom.promise}
+          mouseType={state.mouseType}
+        >
+          {props.overlay}
+        </OverlayMain>
+      </div>
+    );
+  }
 }
 
 Dropdown.defaultProps = {
-    disabled: false, //	菜单是否禁用	boolean	-
-    getContainer: '', //	菜单渲染父节点。默认渲染到 body 上，如果你遇到菜单滚动定位问题，试试修改为滚动的区域，并相对其定位。示例	Function(triggerNode)	() => document.body
-    overlay: '', //	菜单	Menu
-    overlayProps: {},
-    placement: defaultPlacementConfig, //	菜单弹出位置：bottomLeft bottomCenter bottomRight topLeft topCenter topRight String	bottomLeft
-    trigger: 'hover', //	触发下拉的行为	Array<'click'|'hover'>	'hover'
-    defaultVisible: false,
-    visible: undefined, //	菜单是否显示	boolean	-
-    onVisibleChange: '' // 菜单显示状态改变时调用，参数为 visible	Function(visible)	-
+  disabled: false, //	菜单是否禁用	boolean	-
+  getContainer: '', //	菜单渲染父节点。默认渲染到 body 上，如果你遇到菜单滚动定位问题，试试修改为滚动的区域，并相对其定位。示例	Function(triggerNode)	() => document.body
+  overlay: '', //	菜单	Menu
+  overlayProps: {},
+  placement: defaultPlacementConfig, //	菜单弹出位置：bottomLeft bottomCenter bottomRight topLeft topCenter topRight String	bottomLeft
+  trigger: 'hover', //	触发下拉的行为	Array<'click'|'hover'>	'hover'
+  defaultVisible: false,
+  visible: undefined, //	菜单是否显示	boolean	-
+  onVisibleChange: '' // 菜单显示状态改变时调用，参数为 visible	Function(visible)	-
 };
--- src/js/component/menu/index.js
+++ src/js/component/menu/index.js
@@ -13,65 +13,62 @@ import MenuItem from './item';
 import Divider from './divider';
 
 export default class Menu extends Component {
-    constructor(props) {
-        super(props);
-        this.onClick = this.onClick.bind(this);
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick(e) {
+    const { props } = this;
+    if (_.isFunction(props.onClick)) {
+      props.onClick(e);
     }
-    onClick(e) {
-        const { props } = this;
-        if (_.isFunction(props.onClick)) {
-            props.onClick(e);
-        }
-    }
-    get className() {
-        const { props } = this;
-        return classnames([
-            prefixMenu,
-            props.className
-        ]);
-    }
-    get children() {
-        const { selectedKeys, children } = this.props;
-        const _children = _.map(children, (item) => {
-            if (_.some(selectedKeys, val => _.toString(val) === item.key)) {
-                return cloneElement(item, { activ: true });
-            }
-            return item;
-        });
-        return _children;
-    }
-    render() {
-        const { props, mainDom, state } = this;
-        return (
-            <ul
-                // {...props}
-                ref={d => (this.ul = d)}
-                onClick={this.onClick}
-                className={this.className}
-            >
-                {this.children}
-            </ul>
-        );
-    }
+  }
+  get className() {
+    const { props } = this;
+    return classnames([prefixMenu, props.className]);
+  }
+  get children() {
+    const { selectedKeys, children } = this.props;
+    const _children = _.map(children, item => {
+      if (_.some(selectedKeys, val => _.toString(val) === item.key)) {
+        return cloneElement(item, { activ: true });
+      }
+      return item;
+    });
+    return _children;
+  }
+  render() {
+    const { props, mainDom, state } = this;
+    return (
+      <ul
+        // {...props}
+        ref={d => (this.ul = d)}
+        onClick={this.onClick}
+        className={this.className}
+      >
+        {this.children}
+      </ul>
+    );
+  }
 }
 
 Menu.defaultProps = {
-    className: '',
-    defaultOpenKeys: [], //     defaultOpenKeys	初始展开的 SubMenu 菜单项 key 数组
-    defaultSelectedKeys: [], //     defaultSelectedKeys	初始选中的菜单项 key 数组	string[]
-    inlineCollapsed: true, //     inlineCollapsed	inline 时菜单是否收起状态	boolean	-
-    inlineIndent: 24, //     inlineIndent	inline 模式的菜单缩进宽度	number	24
-    mode: 'vertical', //     mode	菜单类型，现在支持垂直、水平、和内嵌模式三种	string: vertical horizontal inline	vertical
-    multiple: false, //     multiple	是否允许多选	boolean	false
-    openKeys: [], //     openKeys	当前展开的 SubMenu 菜单项 key 数组	string[]
-    selectable: true, //     selectable	是否允许选中	boolean	true
-    selectedKeys: [], //     selectedKeys	当前选中的菜单项 key 数组	string[]
-    style: {}, //     style	根节点样式	object
-    theme: 'light', //     theme	主题颜色	string: light dark	light
-    onClick: '', //     onClick	点击 MenuItem 调用此函数	function({ item, key, keyPath })	-
-    onDeselect: '', //     onDeselect	取消选中时调用，仅在 multiple 生效	function({ item, key, selectedKeys })	-
-    onOpenChange: '', //     onOpenChange	SubMenu 展开/关闭的回调	function(openKeys: string[])	noop
-    onSelect: '' //     onSelect	被选中时调用	function({ item, key, selectedKeys })	无
+  className: '',
+  defaultOpenKeys: [], //     defaultOpenKeys	初始展开的 SubMenu 菜单项 key 数组
+  defaultSelectedKeys: [], //     defaultSelectedKeys	初始选中的菜单项 key 数组	string[]
+  inlineCollapsed: true, //     inlineCollapsed	inline 时菜单是否收起状态	boolean	-
+  inlineIndent: 24, //     inlineIndent	inline 模式的菜单缩进宽度	number	24
+  mode: 'vertical', //     mode	菜单类型，现在支持垂直、水平、和内嵌模式三种	string: vertical horizontal inline	vertical
+  multiple: false, //     multiple	是否允许多选	boolean	false
+  openKeys: [], //     openKeys	当前展开的 SubMenu 菜单项 key 数组	string[]
+  selectable: true, //     selectable	是否允许选中	boolean	true
+  selectedKeys: [], //     selectedKeys	当前选中的菜单项 key 数组	string[]
+  style: {}, //     style	根节点样式	object
+  theme: 'light', //     theme	主题颜色	string: light dark	light
+  onClick: '', //     onClick	点击 MenuItem 调用此函数	function({ item, key, keyPath })	-
+  onDeselect: '', //     onDeselect	取消选中时调用，仅在 multiple 生效	function({ item, key, selectedKeys })	-
+  onOpenChange: '', //     onOpenChange	SubMenu 展开/关闭的回调	function(openKeys: string[])	noop
+  onSelect: '' //     onSelect	被选中时调用	function({ item, key, selectedKeys })	无
 };
 
 Menu.Item = MenuItem;
