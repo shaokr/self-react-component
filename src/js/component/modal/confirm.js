396949a28dd2449de18e71ad64f8bee64ff3a8ba
shaokr <shaokangrun@91kook.com>
2018-07-11 15:35:16 +0800

    代码格式

--- src/js/component/modal/confirm.js
+++ src/js/component/modal/confirm.js
@@ -1,4 +1,3 @@
-
 import { Component } from 'react';
 import _ from 'lodash';
 import classnames from 'classnames';
@@ -11,79 +10,77 @@ import './confirm.less';
 const _prefix = `${prefix}-confirm`;
 
 export default class Confirm extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            visible: true
-        };
-        this.onClickKey = this.onClickKey.bind(this);
-        this.hide = this.hide.bind(this);
-    }
-    onClickKey(key, e, col) {
-        const { props } = this;
+    this.state = {
+      visible: true
+    };
+    this.onClickKey = this.onClickKey.bind(this);
+    this.hide = this.hide.bind(this);
+  }
+  onClickKey(key, e, col) {
+    const { props } = this;
 
-        if (_.isFunction(props.onClickKey)) {
-            const res = props.onClickKey(key, e, col);
-            if (_.isUndefined(res) || res) {
-                this.hide();
-            }
-        } else if (col) {
-            this.hide();
-        }
+    if (_.isFunction(props.onClickKey)) {
+      const res = props.onClickKey(key, e, col);
+      if (_.isUndefined(res) || res) {
+        this.hide();
+      }
+    } else if (col) {
+      this.hide();
     }
-    hide() {
-        const { props } = this;
-        props.rdom.remove();
-        this.setState({
-            visible: false
-        });
+  }
+  hide() {
+    const { props } = this;
+    props.rdom.remove();
+    this.setState({
+      visible: false
+    });
+  }
+  get modalProps() {
+    const { props, state } = this;
+    return {
+      title: null,
+      visible: state.visible,
+      onClickKey: this.onClickKey,
+      btn: props.btn,
+      maskClosable: props.maskClosable,
+      rdom: props.rdom
+    };
+  }
+  get calssName() {
+    const { props } = this;
+    return classnames([_prefix, `${_prefix}__${props.type}`]);
+  }
+  get content() {
+    const { props } = this;
+    return (
+      <div className={this.calssName}>
+        <div className={`${_prefix}--title`}>
+          <Icon className={`${_prefix}--icon`} type={props.type} />
+          {props.title}
+        </div>
+        <div className={`${_prefix}--body`}>{props.content}</div>
+      </div>
+    );
+  }
+  render() {
+    const { modalProps } = this;
+    if (!modalProps.visible) {
+      return null;
     }
-    get modalProps() {
-        const { props, state } = this;
-        return {
-            title: null,
-            visible: state.visible,
-            onClickKey: this.onClickKey,
-            btn: props.btn,
-            maskClosable: props.maskClosable,
-            rdom: props.rdom
-        };
-    }
-    get calssName() {
-        const { props } = this;
-        return classnames([
-            _prefix, `${_prefix}__${props.type}`
-        ]);
-    }
-    get content() {
-        const { props } = this;
-        return (
-            <div className={this.calssName}>
-                <div className={`${_prefix}--title`}>
-                    <Icon className={`${_prefix}--icon`} type={props.type} />
-                    {props.title}
-                </div>
-                <div className={`${_prefix}--body`}>{props.content}</div>
-            </div>
-        );
-    }
-    render() {
-        const { modalProps } = this;
-        if (!modalProps.visible) {
-            return null;
-        }
-        return <Modal {...modalProps} >{this.content}</Modal>;
-    }
+    return <Modal {...modalProps}>{this.content}</Modal>;
+  }
 }
 
 Confirm.defaultProps = {
-    btn: [
-        {
-            txt: '确定',
-            type: 'primary',
-            loading: false
-        }
-    ],
-    maskClosable: false
+  btn: [
+    {
+      txt: '确定',
+      type: 'primary',
+      loading: false
+    }
+  ],
+  maskClosable: false
 };
