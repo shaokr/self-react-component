11b3189b861643ae733f4cdd409220c1fba68b6e
shaokr <shaokangrun@91kook.com>
2018-07-31 16:35:13 +0800

    格式代码

--- src/js/component/date-picker/config.js
+++ src/js/component/date-picker/config.js
@@ -1,57 +1,57 @@
 import { prefixDatePicker } from 'config/const';
 
 export const MONTHS = [
-    '1月',
-    '2月',
-    '3月',
-    '4月',
-    '5月',
-    '6月',
-    '7月',
-    '8月',
-    '9月',
-    '10月',
-    '11月',
-    '12月'
+  '1月',
+  '2月',
+  '3月',
+  '4月',
+  '5月',
+  '6月',
+  '7月',
+  '8月',
+  '9月',
+  '10月',
+  '11月',
+  '12月'
 ];
 export const WEEKDAYS_LONG = [
-    '星期日',
-    '星期一',
-    '星期二',
-    '星期三',
-    '星期四',
-    '星期五',
-    '星期六'
+  '星期日',
+  '星期一',
+  '星期二',
+  '星期三',
+  '星期四',
+  '星期五',
+  '星期六'
 ];
 export const WEEKDAYS_SHORT = ['一', '二', '三', '四', '五', '六', '日'];
 export const CLASSNAMES = {
-    container: `${prefixDatePicker}-Container`,
-    wrapper: `${prefixDatePicker}-Wrapper`,
-    interactionDisabled: `${prefixDatePicker}--InteractionDisabled`,
-    navBar: `${prefixDatePicker}-NavBar`,
-    navButton: `${prefixDatePicker}-NavButton`,
-    navButtonPrev: `${prefixDatePicker}-NavButton--prev`,
-    navButtonNext: `${prefixDatePicker}-NavButton--next`,
-    navButtonInteractionDisabled: `${prefixDatePicker}-NavButton--interactionDisabled`,
-    months: `${prefixDatePicker}-Months`,
-    month: `${prefixDatePicker}-Month`,
-    caption: `${prefixDatePicker}-Caption`,
-    weekdays: `${prefixDatePicker}-Weekdays`,
-    weekdaysRow: `${prefixDatePicker}-WeekdaysRow`,
-    weekday: `${prefixDatePicker}-Weekday`,
-    body: `${prefixDatePicker}-Body`,
-    week: `${prefixDatePicker}-Week`,
-    day: `${prefixDatePicker}-Day`,
-    footer: `${prefixDatePicker}-Footer`,
-    todayButton: `${prefixDatePicker}-TodayButton`,
-    today: `${prefixDatePicker}-Day--today`,
-    selected: `${prefixDatePicker}-Day--selected`,
-    disabled: `${prefixDatePicker}-Day--disabled`,
-    outside: `${prefixDatePicker}-Day--outside`
+  container: `${prefixDatePicker}-Container`,
+  wrapper: `${prefixDatePicker}-Wrapper`,
+  interactionDisabled: `${prefixDatePicker}--InteractionDisabled`,
+  navBar: `${prefixDatePicker}-NavBar`,
+  navButton: `${prefixDatePicker}-NavButton`,
+  navButtonPrev: `${prefixDatePicker}-NavButton--prev`,
+  navButtonNext: `${prefixDatePicker}-NavButton--next`,
+  navButtonInteractionDisabled: `${prefixDatePicker}-NavButton--interactionDisabled`,
+  months: `${prefixDatePicker}-Months`,
+  month: `${prefixDatePicker}-Month`,
+  caption: `${prefixDatePicker}-Caption`,
+  weekdays: `${prefixDatePicker}-Weekdays`,
+  weekdaysRow: `${prefixDatePicker}-WeekdaysRow`,
+  weekday: `${prefixDatePicker}-Weekday`,
+  body: `${prefixDatePicker}-Body`,
+  week: `${prefixDatePicker}-Week`,
+  day: `${prefixDatePicker}-Day`,
+  footer: `${prefixDatePicker}-Footer`,
+  todayButton: `${prefixDatePicker}-TodayButton`,
+  today: `${prefixDatePicker}-Day--today`,
+  selected: `${prefixDatePicker}-Day--selected`,
+  disabled: `${prefixDatePicker}-Day--disabled`,
+  outside: `${prefixDatePicker}-Day--outside`
 };
 
 export const CLASSNAMES_INPUT = {
-    container: `${prefixDatePicker}-Container`,
-    overlayWrapper: `${prefixDatePicker}-Input-overlayWrapper`,
-    overlay: `${prefixDatePicker}-Input-overlay`
+  container: `${prefixDatePicker}-Container`,
+  overlayWrapper: `${prefixDatePicker}-Input-overlayWrapper`,
+  overlay: `${prefixDatePicker}-Input-overlay`
 };
--- src/js/component/date-picker/index.js
+++ src/js/component/date-picker/index.js
@@ -8,43 +8,49 @@ import classnames from 'classnames';
 import { prefixDatePicker, prefixInput } from 'config/const';
 
 import './style.less';
-import { MONTHS, WEEKDAYS_LONG, WEEKDAYS_SHORT, CLASSNAMES, CLASSNAMES_INPUT } from './config';
+import {
+  MONTHS,
+  WEEKDAYS_LONG,
+  WEEKDAYS_SHORT,
+  CLASSNAMES,
+  CLASSNAMES_INPUT
+} from './config';
 
 class ThisDayPickerInput extends Component {
-    render() {
-        return <Input ref={el => (this.input = el)} {...this.props} />;
-    }
+  render() {
+    return <Input ref={el => (this.input = el)} {...this.props} />;
+  }
 }
 
 export default class DatePicker extends Component {
-    render() {
-        return (
-            <DayPickerInput
-                component={ThisDayPickerInput}
-                classNames={CLASSNAMES_INPUT}
-                dayPickerProps={{
-                    classNames: CLASSNAMES,
-                    months: MONTHS,
-                    weekdaysLong: WEEKDAYS_LONG,
-                    weekdaysShort: WEEKDAYS_SHORT
-                }}
-            />
-        );
-    }
+  render() {
+    return (
+      <DayPickerInput
+        component={ThisDayPickerInput}
+        classNames={CLASSNAMES_INPUT}
+        dayPickerProps={{
+          classNames: CLASSNAMES,
+          months: MONTHS,
+          weekdaysLong: WEEKDAYS_LONG,
+          weekdaysShort: WEEKDAYS_SHORT
+        }}
+      />
+    );
+  }
 }
 DatePicker.defaultProps = {
-    allowClear: true, //	是否显示清除按钮	boolean	true
-    className: '', //	选择器 className	string	''
-    disabled: false, //	禁用	boolean	false
-    disabledDate: false, //	不可选择的日期	(currentDate: moment) => boolean	无
-    getCalendarContainer: '', //	定义浮层的容器，默认为 body 上新建 div	function(trigger)	无
-    // locale: '', //	国际化配置	object	默认配置
-    open: false, //	控制弹层是否展开	boolean	-
-    placeholder: '', //	输入框提示文字	string|RangePicker[]	-
-    popupStyle: '', //	格外的弹出日历样式	object	{}
-    size: 'default', //	输入框大小，large 高度为 32px，small 为 22px，默认是 28px	string	无
-    style: '', // 自定义输入框样式	object	{}
-    onOpenChange: '' //	弹出日历和关闭日历的回调	function(status)	无
+  allowClear: true, //	是否显示清除按钮	boolean	true
+  className: '', //	选择器 className	string	''
+  disabled: false, //	禁用	boolean	false
+  disabledDate: false, //	不可选择的日期	(currentDate: moment) => boolean	无
+  getCalendarContainer: '', //	定义浮层的容器，默认为 body 上新建 div	function(trigger)	无
+  // locale: '', //	国际化配置	object	默认配置
+  open: false, //	控制弹层是否展开	boolean	-
+  placeholder: '', //	输入框提示文字	string|RangePicker[]	-
+  popupStyle: '', //	格外的弹出日历样式	object	{}
+  size: 'default', //	输入框大小，large 高度为 32px，small 为 22px，默认是 28px	string	无
+  style: '', // 自定义输入框样式	object	{}
+  onOpenChange: '' //	弹出日历和关闭日历的回调	function(status)	无
 };
 // import { DatePicker } from 'antd';
 
