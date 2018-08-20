b245dec7e08262165a05ee62b977701e9a1f5cd9
shaokr <shaokangrun@91kook.com>
2018-08-20 11:50:49 +0800

    格式化

--- src/js/component/tree/selected-list.js
+++ src/js/component/tree/selected-list.js
@@ -12,36 +12,47 @@ import { Checked } from './circle';
 // const { CSSTransitionGroup } = addons;
 
 class Li extends Component {
-    shouldComponentUpdate() {
-        return false;
-    }
-    get small() {
-        const key = _.get(this, ['props', 'data', 'key']);
-        return _.get(this, ['props', 'store', 'list', key, 'self', 'small']);
-    }
-    render() {
-        const { data, action } = this.props;
-		// 是否可删除
-        return (
-            <li>
-                <div className="tree-selected-front">
-                    <Avatar name={data.name} avatar={data.avatar} dataKey={data.key} color={data.color} icon={data.icon} />
-                </div>
-                <div className="tree-selected-name">
-                    <p>{data.name}</p>
-                    <small>{this.small}</small>
-                </div>
-                <Checked show={data.isDel} type="-1" onClick={() => action.hasSelectedItem(data)} />
-            </li>
-        );
-    }
+  shouldComponentUpdate() {
+    return false;
+  }
+  get small() {
+    const key = _.get(this, ['props', 'data', 'key']);
+    return _.get(this, ['props', 'store', 'list', key, 'self', 'small']);
+  }
+  render() {
+    const { data, action } = this.props;
+    // 是否可删除
+    return (
+      <li>
+        <div className="tree-selected-front">
+          <Avatar
+            name={data.name}
+            avatar={data.avatar}
+            dataKey={data.key}
+            color={data.color}
+            icon={data.icon}
+          />
+        </div>
+        <div className="tree-selected-name">
+          <p>{data.name}</p>
+          <small>{this.small}</small>
+        </div>
+        <Checked
+          show={data.isDel}
+          type="-1"
+          onClick={() => action.hasSelectedItem(data)}
+        />
+      </li>
+    );
+  }
 }
 
-
 export default ({ data, store, action }) => (
-    <div className="scroll">
-        <ul className="tree-selected-ul">
-            { _.map(data, item => <Li key={item.key} data={item} action={action} store={store} />) }
-        </ul>
-    </div>
+  <div className="scroll">
+    <ul className="tree-selected-ul">
+      {_.map(data, item => (
+        <Li key={item.key} data={item} action={action} store={store} />
+      ))}
+    </ul>
+  </div>
 );
