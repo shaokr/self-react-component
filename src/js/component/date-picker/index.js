694cbca2a045d8e1b56dd0b1a047634ccd76e37a
shaokr <shaokangrun@91kook.com>
2018-11-09 10:25:46 +0800

    代码优化

--- src/js/component/date-picker/index.js
+++ src/js/component/date-picker/index.js
@@ -47,20 +47,21 @@ export default class extends Component {
     allowClear: true, //	是否显示清除按钮	boolean	true
     className: '', //	选择器 className	string	''
     disabled: false, //	禁用	boolean	false
-    // disabledDate: '', //	不可选择的日期	(currentDate: moment) => boolean	无
-    // disabledDateTime: '',
+    disabledDate: '', //	不可选择的日期	(currentDate: moment) => boolean	无
+    disabledDateTime: '',
     minDate: '', // 最小可选时间
     maxDate: '', // 最大可选时间
-    getCalendarContainer: '', //	定义浮层的容器，默认为 body 上新建 div	function(trigger)	无
+    // getCalendarContainer: '', //	定义浮层的容器，默认为 body 上新建 div	function(trigger)	无
     defaultValue: +new Date(),
-    // showTime: false,
+    showTime: false,
     // locale: '', //	国际化配置	object	默认配置
     // open: false, //	控制弹层是否展开	boolean	-
     // placeholder: lang.inputPlaceholder, //	输入框提示文字	string|RangePicker[]	-
-    popupStyle: '', //	格外的弹出日历样式	object	{}
+    // popupStyle: '', //	格外的弹出日历样式	object	{}
     size: 'default', //	输入框大小，large 高度为 32px，small 为 22px，默认是 28px	string	无
-    style: '' // 自定义输入框样式	object	{}
-    // onOpenChange: '' //	弹出日历和关闭日历的回调	function(status)	无
+    style: '', // 自定义输入框样式	object	{}
+    onOpenChange() {}, //	弹出日历和关闭日历的回调	function(status)	无
+    onChange() {}
   };
   constructor(props) {
     super(props);
